d74c337558a56ab1a4305700eb1177d631915feb
Suong Tran <suong.tran@oivan.com>
2023-04-23 03:45:50 +0700

    fix

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module MovieApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.api_only = true
+    config.assets.initialize_on_precompile = false
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
