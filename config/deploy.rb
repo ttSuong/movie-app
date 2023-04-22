c2b61502241d525fa9a4932adf16a6741195e1e3
Suong Tran <suong.tran@oivan.com>
2023-04-23 03:40:49 +0700

    add platform x86_64-linux

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,24 @@ set :repo_url, "git@github.com:ttSuong/movie-app.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+# config valid only for current version of Capistrano
+set :pty, true
+set :linked_files, %w(config/database.yml config/application.yml)
+set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
+set :keep_releases, 5
+set :rvm_type, :user
+
+set :puma_rackup, -> {File.join(current_path, "config.ru")}
+set :puma_state, -> {"#{shared_path}/tmp/pids/puma.state"}
+set :puma_pid, -> {"#{shared_path}/tmp/pids/puma.pid"}
+set :puma_bind, -> {"unix://#{shared_path}/tmp/sockets/puma.sock"}
+set :puma_conf, -> {"#{shared_path}/puma.rb"}
+set :puma_access_log, -> {"#{shared_path}/log/puma_access.log"}
+set :puma_error_log, -> {"#{shared_path}/log/puma_error.log"}
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, "staging"))
+set :puma_threads, [0, 8]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+set :puma_preload_app, false
