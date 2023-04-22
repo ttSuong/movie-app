08dcab73d22908796bcaa2a108b169dbb453bbd7
Suong Tran <suong.tran@oivan.com>
2023-04-23 04:50:01 +0700

    Fix bug

--- app/services/users/create_session.rb
+++ app/services/users/create_session.rb
@@ -12,7 +12,7 @@ module Users
         session = User.authenticate(@user_params[:email], @user_params[:password])
         raise I18n.t("user.errors.invalid_password") unless session.present?
       else
-        user = User.create(email: @user_params[:email], password: @user_params[:password])
+        user = User.create!(email: @user_params[:email], password: @user_params[:password])
         raise I18n.t("user.errors.error_register") unless user.present?
       end
       Api::GenerateTokenService.new(user, @client_id).call
